c0875761e21ba3667f15c4f1c2d87ebaffaf413f
Poon Yip Hoon (Aeric) <yiphoon@yahoo.com>
2026-05-04 02:41:34 +0800

    create b4xlib

---
+++ source/convert.bat
@@ -0,0 +1,4 @@
+copy ..\AuthFilter.bas ..\AuthFilter.b4x_excluded
+copy ..\AuthHandler.bas ..\AuthHandler.b4x_excluded
+copy ..\AuthView.bas ..\AuthView.b4x_excluded
+copy ..\UsersModel.bas ..\UsersModel.b4x_excluded
